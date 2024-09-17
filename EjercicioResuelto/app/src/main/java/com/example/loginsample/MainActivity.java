e931ba4cd2593f6843078c87bcdc03e6e83aecca
eazurin <eazurin@unsa.edu.pe>
2024-09-17 10:56:54 -0500

    Ejercicio propuesto: Inicializacion de la interfaz y los componentes

--- EjercicioResuelto/app/src/main/java/com/example/loginsample/MainActivity.java
+++ EjercicioResuelto/app/src/main/java/com/example/loginsample/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.loginsample;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +11,10 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.loginsample.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +26,22 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         }); */
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        EditText edtUsername = binding.edtUsername;
+        EditText edtPassword = binding.edtPassword;
+        Button btnLogin = binding.btnLongin;
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+
     }
 }
