67d0271ca6917d809a31635c55ec8adc24bdd3fe
eazurin <eazurin@unsa.edu.pe>
2024-09-17 13:29:51 -0500

    Ejercicio propuesto: Inicializando los componentes

--- EjercicioPropuesto/app/src/main/java/com/example/appregistration/MainActivity.java
+++ EjercicioPropuesto/app/src/main/java/com/example/appregistration/MainActivity.java
@@ -1,6 +1,13 @@
 package com.example.appregistration;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +15,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.example.appregistration.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         /*       EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
@@ -21,5 +30,24 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         }); */
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+        EditText edtName = binding.edtName;
+        EditText edtLastname = binding.edtLastname;
+        EditText edtPhone = binding.edtPhone;
+        EditText edtBloodGroup = binding.edtBloodGroup;
+        EditText edtAdress = binding.editAddress;
+        Button btnRegistration = binding.btnRegistration;
+        Button btnView = binding.btnView;
+
+        btnRegistration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
